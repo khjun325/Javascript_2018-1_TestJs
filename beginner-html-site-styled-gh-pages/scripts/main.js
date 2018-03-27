408c055ec6436bbae565d7b5bfba9014ebe16c48
paul <khjun325@naver.com>
2018-03-27 10:49:47 +0900

    [2018.0..27]

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,10 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Cloude Service<br>2018-1';
+var text = 'This is a text.';
+text =10.5;
+var a = 33.7;
+var b = a*text;
+// alert(b);  //팝업 띄우기
+var iceCream = 'chocolate';
+var id = prompt('ID 입력',''); //prompt는 입력창
+alert(id+'가 로그인함.');
---
+++ getComPush.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "[2018.0..27]"
+git push -u origin master
